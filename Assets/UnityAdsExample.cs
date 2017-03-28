9c784ed0e35311ab228485e32f1c38486aaa9890
mentor-oonuki <mentor.oonuki@gmail.com>
2017-03-28 11:03:33 +0900

    【初期設定】 UnityAds サンプル

---
+++ Assets/UnityAdsExample.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class UnityAdsExample : MonoBehaviour
+{
+
+	[SerializeField]
+	private string androidGameId = "18658"; //ID for testing
+	[SerializeField]
+	private string iosGameId = "18660"; //ID for testing
+
+	[SerializeField]
+	bool enableTestMode;
+
+	void Start()
+	{
+		string gameId = null;
+
+#if UNITY_IOS // If build platform is set to iOS...
+        gameId = iosGameId;
+#elif UNITY_ANDROID // Else if build platform is set to Android...
+		gameId = androidGameId;
+#endif
+
+		if (string.IsNullOrEmpty(gameId))
+		{ // Make sure the Game ID is set.
+			Debug.LogError("Failed to initialize Unity Ads. Game ID is null or empty.");
+		}
+		else if (!Advertisement.isSupported)
+		{
+			Debug.LogWarning("Unable to initialize Unity Ads. Platform not supported.");
+		}
+		else if (Advertisement.isInitialized)
+		{
+			Debug.Log("Unity Ads is already initialized.");
+		}
+		else
+		{
+			Debug.Log(string.Format("Initialize Unity Ads using Game ID {0} with Test Mode {1}.",
+				gameId, enableTestMode ? "enabled" : "disabled"));
+			Advertisement.Initialize(gameId, enableTestMode);
+		}
+	}
+}
---
+++ Assets/UnityAdsRewardedButton.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class UnityAdsRewardedButton : MonoBehaviour
+{
+	public string zoneId;
+	public int rewardQty = 250;
+
+	void OnGUI()
+	{
+		if (string.IsNullOrEmpty(zoneId)) zoneId = null;
+
+		Rect buttonRect = new Rect(10, 10, 150, 50);
+		string buttonText = Advertisement.IsReady(zoneId) ? "Show Ad" : "Waiting...";
+
+		ShowOptions options = new ShowOptions();
+		options.resultCallback = HandleShowResult;
+
+		if (GUI.Button(buttonRect, buttonText))
+		{
+			Advertisement.Show(zoneId, options);
+		}
+	}
+
+	private void HandleShowResult(ShowResult result)
+	{
+		switch (result)
+		{
+			case ShowResult.Finished:
+				Debug.Log("Video completed. User rewarded " + rewardQty + " credits.");
+				break;
+			case ShowResult.Skipped:
+				Debug.LogWarning("Video was skipped.");
+				break;
+			case ShowResult.Failed:
+				Debug.LogError("Video failed to show.");
+				break;
+		}
+	}
+}
